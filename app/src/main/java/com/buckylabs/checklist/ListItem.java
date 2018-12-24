f4f7a85d59c9d2b7bb52590f9e16e14bdb6b8760
devbuckylabs <dev.buckylabs@gmail.com>
2018-12-24 19:41:54 +0530

    Added - boolean isItemChecked variable to hold checkbox state

--- app/src/main/java/com/buckylabs/checklist/ListItem.java
+++ app/src/main/java/com/buckylabs/checklist/ListItem.java
@@ -4,9 +4,11 @@ package com.buckylabs.checklist;
 public class ListItem {
 
     private String item;
+    private boolean isItemChecked;
 
-    public ListItem(String item) {
+    public ListItem(String item, boolean isItemChecked) {
         this.item = item;
+        this.isItemChecked = isItemChecked;
     }
 
     public String getItem() {
@@ -17,10 +19,19 @@ public class ListItem {
         this.item = item;
     }
 
+    public boolean isItemChecked() {
+        return isItemChecked;
+    }
+
+    public void setItemChecked(boolean itemChecked) {
+        isItemChecked = itemChecked;
+    }
+
     @Override
     public String toString() {
         return "ListItem{" +
                 "item='" + item + '\'' +
+                ", isItemChecked=" + isItemChecked +
                 '}';
     }
 }
