c25df6361624354f0d633111b12b5d273d88e1d1
devbuckylabs <dev.buckylabs@gmail.com>
2018-12-24 11:25:21 +0530

    Added - addListItemToDB() Adding checkbox state (ALL LIST<LISTITEM)to db on everyclick. Not the best but it works. While optimizing i'll change.

--- app/src/main/java/com/buckylabs/checklist/ListItem_Adapter.java
+++ app/src/main/java/com/buckylabs/checklist/ListItem_Adapter.java
@@ -1,20 +1,28 @@
 package com.buckylabs.checklist;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListItem_Adapter extends RecyclerView.Adapter<ListItem_Adapter.ViewHolder> {
 
     private List<ListItem> itemList;
     private Context context;
+    private DatabaseHelper databaseHelper;
+    private List<ListItem> updateListItems = new ArrayList<>();
 
     public ListItem_Adapter(List<ListItem> listItems, Context context) {
         this.context = context;
@@ -25,15 +33,35 @@ public class ListItem_Adapter extends RecyclerView.Adapter<ListItem_Adapter.View
     @Override
     public ListItem_Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false);
+        databaseHelper = new DatabaseHelper(context);
         return new ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
 
-        ListItem listItem = itemList.get(i);
-        Log.e("Bind", listItem.toString());
+        final ListItem listItem = itemList.get(i);
+
         viewHolder.textView.setText(listItem.getItem());
+        viewHolder.checkBox.setChecked(listItem.isItemChecked());
+        Log.e("Bind", listItem.toString());
+        viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                viewHolder.checkBox.setChecked((isChecked));
+                Log.e("onCheckChanged", viewHolder.checkBox.isChecked() + " ");
+                listItem.setItemChecked(viewHolder.checkBox.isChecked());
+                updateListItems.clear();
+                updateListItems.addAll(itemList);
+                //updateListItems.add(listItem);
+                Log.e("onCheckChanged", listItem.toString());
+                int id = ((Activity) context).getIntent().getExtras().getInt("ID");
+                Log.e("ID", id + "");
+                Category category = databaseHelper.getCategory(1);
+                Log.e("Printing again", category.toString());
+                viewHolder.addListItemToDB(id);
+            }
+        });
 
     }
 
@@ -44,10 +72,25 @@ public class ListItem_Adapter extends RecyclerView.Adapter<ListItem_Adapter.View
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView textView;
+        public CheckBox checkBox;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.listitem_textView);
+            checkBox = itemView.findViewById(R.id.checkBox);
+        }
+
+        public void addListItemToDB(int id) {
+            Category category = databaseHelper.getCategory(id);
+            category.setListItems(updateListItems);
+            boolean res = databaseHelper.updatedbdata(category);
+            if (res) {
+                Log.e("update", "row update complete");
+            } else {
+                Log.e("update", "row update failed");
+
+            }
+
         }
     }
 }
